72f1e7a10fa0fce03d59cce777fd0791fef403c8
FlorianBieck <florian@bieck.com>
2018-07-02 22:58:16 +0200

    Car Model Fix

--- src/main/java/com/fbieck/repositories/carmodel/EmissionClassRepository.java
+++ src/main/java/com/fbieck/repositories/carmodel/EmissionClassRepository.java
@@ -3,8 +3,5 @@ package com.fbieck.repositories.carmodel;
 import com.fbieck.entities.carmodel.EmissionClass;
 import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.Entity;
-
-@Entity
 public interface EmissionClassRepository extends CrudRepository<EmissionClass, String> {
 }
